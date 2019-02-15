42b37eb57048694d689a3d95ccd70595843dd189
unknown <mzinnocents96@gmail.com>
2019-02-15 12:01:37 -0600

    heroku

---
+++ DB.sql
@@ -0,0 +1,18 @@
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `t_users`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_users`;
+CREATE TABLE `t_users` (
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_users
+-- ----------------------------
+INSERT INTO `t_users` VALUES ('1', 'Bonjour', 'mondeEEEEE');
+INSERT INTO `t_users` VALUES ('2', 'Test2', 'lastnameTest');
+INSERT INTO `t_users` VALUES ('3', 'test3', 'LastNameTest3');
